e0ca49a5e990d0fb29e5404fdec9d723948df3d0
gserrano <gserrano@switchfly.com>
2015-12-23 20:52:11 +0000

    merge sort

--- src/test/groovy/mergesort/MergeSortSpec.groovy
+++ src/test/groovy/mergesort/MergeSortSpec.groovy
@@ -1,24 +1,27 @@
 package mergesort
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 
 class MergeSortSpec extends Specification {
-    def "MergeSort.sort(List<Integer>) returns a new List<Integer> with elements sorted"() {
+    @Unroll
+    def "MergeSort.sort(#numbers) returns a new list with its elements sorted #expected"() {
         expect: "e.g. MergeSort.sort([3, 2, 1]) returning [1, 2, 3]"
         MergeSort.sort(numbers) == expected
 
         where:
-        numbers      | expected
-        null         | []
-        [1]          | [1]
-        [2]          | [2]
-        []           | []
-        [1, 0]       | [0, 1]
-        [2, 1]       | [1, 2]
-        [0, 1]       | [0, 1]
-        [1, 3, 2, 4] | [1, 2, 3, 4]
-        [3, 2, 1]    | [1, 2, 3]
-        [4, 3, 2, 1] | [1, 2, 3, 4]
+        numbers                       | expected
+        null                          | []
+        [1]                           | [1]
+        [2]                           | [2]
+        []                            | []
+        [1, 0]                        | [0, 1]
+        [2, 1]                        | [1, 2]
+        [0, 1]                        | [0, 1]
+        [1, 3, 2, 4]                  | [1, 2, 3, 4]
+        [3, 2, 1]                     | [1, 2, 3]
+        [4, 3, 2, 1]                  | [1, 2, 3, 4]
+        [2, 5, 2, 6, 1, -2, 0, 44, 0] | [-2, 0, 0, 1, 2, 2, 5, 6, 44]
     }
 }
