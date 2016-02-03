0caf0a168074ea259c6a94f6d9f6d6cd0e0c9ad2
gserrano <gserrano@switchfly.com>
2016-02-03 02:41:20 +0000

    finished

--- src/main/java/lis/Lis.java
+++ src/main/java/lis/Lis.java
@@ -1,19 +1,52 @@
 package lis;
 
+import java.util.Arrays;
+
 public class Lis {
     public static int calculate(int[] numbers) {
-        if (numbers.length == 0)
+        int length = numbers.length;
+
+        if (length == 0)
             return 0;
-        if (numbers.length == 1)
+        if (length == 1)
             return 1;
 
-        int index = 0;
-        int lis = 1;
+        int[] lis = arrayOf(1, length);
+        int[] top = new int[length];
 
-        while (index + 1 < numbers.length)
-            if (numbers[index] < numbers[index++ + 1])
-                lis++;
+        top[0] = numbers[0];
+        int sequenceId = 1;
 
-        return lis;
+        for (int index = 0; index + 1 < length; index++) {
+            int next = numbers[index + 1];
+            int sequenceIndex = 0;
+
+            sequenceIndex = getSequenceIndex(top, sequenceId, next, sequenceIndex);
+
+            if (sequenceIndex < sequenceId) {
+                top[sequenceIndex] = next;
+                lis[sequenceIndex]++;
+            } else if (sequenceId + 1 < length) {
+                top[sequenceId++] = next;
+            }
+        }
+
+        Arrays.sort(lis);
+
+        return lis[length - 1];
+    }
+
+    protected static int getSequenceIndex(int[] top, int sequenceId, int next, int sequenceIndex) {
+        for (; sequenceIndex < sequenceId; sequenceIndex++)
+            if (top[sequenceIndex] < next)
+                return sequenceIndex;
+
+        return sequenceId;
+    }
+
+    private static int[] arrayOf(int initialValue, int length) {
+        int[] ints = new int[length];
+        Arrays.fill(ints, initialValue);
+        return ints;
     }
 }
--- src/test/java/lis/LisTest.java
+++ src/test/java/lis/LisTest.java
@@ -10,11 +10,14 @@ public class LisTest {
     @Test
     public void testCalculate() {
         assertEquals(0, Lis.calculate(new int[0])); // empty collection
-        assertEquals(1, Lis.calculate(new int[] {0})); // single element
-        assertEquals(2, Lis.calculate(new int[] {0, 1})); // collection bigger than one
-        assertEquals(1, Lis.calculate(new int[] {1, 0})); // sequence smaller than collection
-        assertEquals(3, Lis.calculate(new int[] {0, 1, 2})); // more than 2 in sequence
-        assertEquals(2, Lis.calculate(new int[] {0, 1, 1})); // break the sequence
-        assertEquals(2, Lis.calculate(new int[] {1, 0, 2})); // noise in sequence
+        assertEquals(1, Lis.calculate(new int[]{0})); // single element
+        assertEquals(2, Lis.calculate(new int[]{0, 1})); // collection bigger than one
+        assertEquals(1, Lis.calculate(new int[]{1, 0})); // sequence smaller than collection
+        assertEquals(3, Lis.calculate(new int[]{0, 1, 2})); // more than 2 in sequence
+        assertEquals(2, Lis.calculate(new int[]{0, 1, 1})); // break the sequence
+        assertEquals(2, Lis.calculate(new int[]{0, 1, 0, 1})); // noise in sequence
+        assertEquals(3, Lis.calculate(new int[]{5, 1, 6, 2, 3})); // sequence of smaller numbers is greater
+        assertEquals(3, Lis.calculate(new int[]{1, -5, 2, -9, -4, -8, -7})); // negative numbers
+//        assertEquals(4, Lis.calculate(new int[]{2, 5, 3, 7, 101, 18})); //
     }
 }
