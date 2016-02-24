ddf7cd410970a50e99f0d25dc352f38520d38462
gserrano <gserrano@switchfly.com>
2016-02-24 22:41:45 +0000

    more readable implementation

--- src/main/java/caesarcipher/Caesar.java
+++ src/main/java/caesarcipher/Caesar.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Caesar {
     public static final int ALPHABET_LENGTH = 'z' - 'a';
+    public static final int CASE_INCREMENT = 'a' - 'A';
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,13 +14,25 @@ public class Caesar {
 
     public static String encrypt(String s, int k) {
         char[] chars = s.toCharArray();
-        for (int index = 0; index < chars.length; index++) if (isConvertible(chars[index]))
-                chars[index] = encryptChar(chars[index], k);
+        for (int index = 0; index < chars.length; index++)
+            chars[index] = (char) (encryptIfConvertible(toLower(chars[index]), k) - lowerBonus(chars[index]));
         return new String(chars);
     }
 
-    protected static boolean isConvertible(char c) {
-        return isLower(c) || isUpper(c);
+    protected static char encryptIfConvertible(char c, int k) {
+        return isLower(c) ? encryptChar(c, k) : c;
+    }
+
+    protected static char encryptChar(char c, int k) {
+        return (char) ((c - 'a' + k) % (ALPHABET_LENGTH + 1) + 'a');
+    }
+
+    protected static int lowerBonus(char c) {
+        return isUpper(c) ? CASE_INCREMENT : 0;
+    }
+
+    protected static char toLower(char c) {
+        return isUpper(c) ? (char) (c + CASE_INCREMENT) : c;
     }
 
     private static boolean isUpper(char c) {
@@ -28,10 +41,5 @@ public class Caesar {
 
     private static boolean isLower(char c) {
         return 'a' <= c && c <= 'z';
-    }
-
-    protected static char encryptChar(char c, int k) {
-        char base = isLower(c) ? 'a' : 'A';
-        return (char) ((c - base + k) % (ALPHABET_LENGTH + 1) + base);
     }
 }
