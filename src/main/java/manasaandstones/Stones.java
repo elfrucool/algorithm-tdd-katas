ae8d598147f4424b28d51d398af1ef1b05636fe2
gserrano <gserrano@switchfly.com>
2016-03-01 21:50:11 +0000

    better approach

--- src/main/java/manasaandstones/Stones.java
+++ src/main/java/manasaandstones/Stones.java
@@ -6,12 +6,14 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
+import static java.util.Collections.singleton;
+
 public class Stones {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfTestCases = scanner.nextInt();
         while (numberOfTestCases-- > 0)
-            System.out.println(join(getLast(scanner.nextInt() - 1, scanner.nextInt(), scanner.nextInt())));
+            System.out.println(join(getLast(scanner.nextInt(), scanner.nextInt(), scanner.nextInt())));
     }
 
     private static String join(Collection<Integer> numbers) {
@@ -21,9 +23,14 @@ public class Stones {
     public static Set<Integer> getLast(int n, int a, int b) {
         Set<Integer> lastStones = new TreeSet<>();
 
-        for (int i = n; i >= 0; i--)
-            lastStones.add(a * i + b * (n - i));
+        if (n == 1)
+            return singleton(0);
+
+        int zeroBasedN = n - 1;
+        for (int i = zeroBasedN; i >= 0; i--)
+            lastStones.add(a * i + b * (zeroBasedN - i));
 
         return lastStones;
     }
 }
+
--- src/test/java/manasaandstones/StonesTest.java
+++ src/test/java/manasaandstones/StonesTest.java
@@ -12,13 +12,14 @@ import static org.junit.Assert.assertEquals;
 public class StonesTest {
     @Test
     public void testGetLast() {
-        assertEquals(singleton(1), Stones.getLast(1, 1, 1)); // single stone, single value
-        assertEquals(singleton(2), Stones.getLast(1, 2, 2)); // single stone, single value (2)
-        assertEquals(singleton(2), Stones.getLast(2, 1, 1)); // two stones, single value
-        assertEquals(asSet(1, 2), Stones.getLast(1, 1, 2)); // single stone two values
-        assertEquals(asSet(2, 3, 4), Stones.getLast(2, 1, 2)); // two stones two values
-        assertEquals(asSet(3, 4, 5, 6), Stones.getLast(3, 1, 2)); // three stones, two values
-        assertEquals(asSet(3, 4, 5, 6), Stones.getLast(3, 2, 1)); // three stones, two values, first is bigger
+        assertEquals(singleton(0), Stones.getLast(1, 1, 1)); // single stone, single value
+        assertEquals(singleton(1), Stones.getLast(2, 1, 1)); // two stone, single value
+        assertEquals(singleton(2), Stones.getLast(2, 2, 2)); // two stone, single value (2)
+        assertEquals(singleton(2), Stones.getLast(3, 1, 1)); // three stones, single value
+        assertEquals(asSet(1, 2), Stones.getLast(2, 1, 2)); // two stones two values
+        assertEquals(asSet(2, 3, 4), Stones.getLast(3, 1, 2)); // three stones two values
+        assertEquals(asSet(3, 4, 5, 6), Stones.getLast(4, 1, 2)); // four stones, two values
+        assertEquals(asSet(3, 4, 5, 6), Stones.getLast(4, 2, 1)); // four stones, two values, first is bigger
     }
 
     private Set<Integer> asSet(Integer... numbers) {
