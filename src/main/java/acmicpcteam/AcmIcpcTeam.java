e1ed5b3c0095a878fd4651400b961af94042323f
gserrano <gserrano@switchfly.com>
2016-03-03 18:26:39 +0000

    basic structure

--- src/main/java/acmicpcteam/AcmIcpcTeam.java
+++ src/main/java/acmicpcteam/AcmIcpcTeam.java
@@ -1,28 +1,56 @@
 package acmicpcteam;
 
 import java.util.BitSet;
+import java.util.List;
 
 public class AcmIcpcTeam {
+    public static Results calculate(List<BitObject> peopleWithKnowledge) {
+        int teams = 0;
+        int pivot = 0;
+        int topics = 0;
+        int numberOfPeople = peopleWithKnowledge.size();
+
+        if (pivot + 1 < numberOfPeople) {
+            int offset = 1;
+            while (offset < numberOfPeople) {
+                topics = topicsOfTeam(peopleWithKnowledge.get(pivot), peopleWithKnowledge.get(pivot + offset));
+                offset++;
+                teams++;
+            }
+        }
+
+        return results(topics, teams);
+    }
+
+    public static int topicsOfTeam(BitObject person1, BitObject person2) {
+        BitSet result = (BitSet) person1.getBitSet().clone();
+        result.or(person2.getBitSet());
+        return result.cardinality();
+    }
 
     public static String bitsToString(BitObject bitObject) {
-        String s = "";
+        StringBuilder s = new StringBuilder();
         for (int i = 0; i < bitObject.getSize(); i++)
-            s += bitObject.getBitSet().get(i) ? "1" : "0";
-        return s;
+            s.append(bitObject.getBitSet().get(i) ? "1" : "0");
+        return s.toString();
     }
 
     public static BitObject stringToBits(String bitsStr) {
         int size = bitsStr.length();
         BitSet bitSet = new BitSet(size);
 
-        for(int index = 0; index < size; index++)
-          bitSet.set(index, bitsStr.charAt(index) == '1');
+        for (int index = 0; index < size; index++)
+            bitSet.set(index, bitsStr.charAt(index) == '1');
 
         return bitObject(size, bitSet);
     }
 
     public static BitObject bitObject(int size, BitSet bitSet) {
         return new BitObject(size, bitSet);
+    }
+
+    public static Results results(int topics, int teams) {
+        return new Results(topics, teams);
     }
 
     public static class BitObject {
@@ -40,6 +68,41 @@ public class AcmIcpcTeam {
         public BitObject(int size, BitSet bitSet) {
             this.size = size;
             this.bitSet = bitSet;
+        }
+    }
+
+    public static class Results {
+        private final int topics;
+        private final int teams;
+
+        @Override
+        public String toString() {
+            return "Results{" +
+                    "topics=" + topics +
+                    ", teams=" + teams +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Results results = (Results) o;
+
+            return topics == results.topics && teams == results.teams;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = topics;
+            result = 31 * result + teams;
+            return result;
+        }
+
+        public Results(int topics, int teams) {
+            this.topics = topics;
+            this.teams = teams;
         }
     }
 }
--- src/test/java/acmicpcteam/AcmIcpcTeamTest.java
+++ src/test/java/acmicpcteam/AcmIcpcTeamTest.java
@@ -4,30 +4,48 @@ import org.junit.Test;
 
 import java.util.BitSet;
 
-import static acmicpcteam.AcmIcpcTeam.bitObject;
+import static acmicpcteam.AcmIcpcTeam.*;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class AcmIcpcTeamTest {
-    public static final String BIG_BITS = "11101111101111101111101111101111101111101111101111101111101100000000001110110";
+    public static final String BIG_BITS = "111101111101111101111101111101111101111101111101111101100000000001110110";
+
+    @Test
+    public void testCalculate() {
+        assertEquals(results(0, 1), calculate(asList(stringToBits("0"), stringToBits("0"))));
+        assertEquals(results(1, 1), calculate(asList(stringToBits("1"), stringToBits("0"))));
+        assertEquals(results(1, 2), calculate(asList(stringToBits("0"), stringToBits("0"), stringToBits("1"))));
+    }
+
+    @Test
+    public void testTopicsOfTeam() {
+        assertEquals(0, topicsOfTeam(stringToBits(""), stringToBits("")));
+        assertEquals(1, topicsOfTeam(stringToBits("1"), stringToBits("0")));
+        assertEquals(1, topicsOfTeam(stringToBits("0"), stringToBits("1")));
+        assertEquals(2, topicsOfTeam(stringToBits("10"), stringToBits("01")));
+        assertEquals(1, topicsOfTeam(stringToBits("10"), stringToBits("10")));
+        assertEquals(5, topicsOfTeam(stringToBits("11011"), stringToBits("10101")));
+    }
 
     @Test
     public void testBitsToString() {
-        assertEquals("", AcmIcpcTeam.bitsToString(bitObject(0, BitSet.valueOf(new byte[0]))));
-        assertEquals("1", AcmIcpcTeam.bitsToString(bitObject(1, BitSet.valueOf(new byte[]{0b1}))));
-        assertEquals("0", AcmIcpcTeam.bitsToString(bitObject(1, BitSet.valueOf(new byte[]{0b0}))));
-        assertEquals("01", AcmIcpcTeam.bitsToString(bitObject(2, BitSet.valueOf(new byte[]{0b10}))));
-        assertEquals("10", AcmIcpcTeam.bitsToString(bitObject(2, BitSet.valueOf(new byte[]{0b01}))));
-        assertEquals("11101", AcmIcpcTeam.bitsToString(bitObject(5, BitSet.valueOf(new byte[]{0b10111}))));
-        assertEquals("1110110", AcmIcpcTeam.bitsToString(bitObject(7, BitSet.valueOf(new byte[]{0b0110111}))));
+        assertEquals("", bitsToString(bitObject(0, BitSet.valueOf(new byte[0]))));
+        assertEquals("1", bitsToString(bitObject(1, BitSet.valueOf(new byte[]{0b1}))));
+        assertEquals("0", bitsToString(bitObject(1, BitSet.valueOf(new byte[]{0b0}))));
+        assertEquals("01", bitsToString(bitObject(2, BitSet.valueOf(new byte[]{0b10}))));
+        assertEquals("10", bitsToString(bitObject(2, BitSet.valueOf(new byte[]{0b01}))));
+        assertEquals("11101", bitsToString(bitObject(5, BitSet.valueOf(new byte[]{0b10111}))));
+        assertEquals("1110110", bitsToString(bitObject(7, BitSet.valueOf(new byte[]{0b0110111}))));
     }
 
     @Test
     public void testStringToBits() {
-        assertEquals("", AcmIcpcTeam.bitsToString(AcmIcpcTeam.stringToBits("")));
-        assertEquals("1", AcmIcpcTeam.bitsToString(AcmIcpcTeam.stringToBits("1")));
-        assertEquals("0", AcmIcpcTeam.bitsToString(AcmIcpcTeam.stringToBits("0")));
-        assertEquals("11", AcmIcpcTeam.bitsToString(AcmIcpcTeam.stringToBits("11")));
-        assertEquals("1110110", AcmIcpcTeam.bitsToString(AcmIcpcTeam.stringToBits("1110110")));
-        assertEquals(BIG_BITS, AcmIcpcTeam.bitsToString(AcmIcpcTeam.stringToBits(BIG_BITS)));
+        assertEquals("", bitsToString(AcmIcpcTeam.stringToBits("")));
+        assertEquals("1", bitsToString(AcmIcpcTeam.stringToBits("1")));
+        assertEquals("0", bitsToString(AcmIcpcTeam.stringToBits("0")));
+        assertEquals("11", bitsToString(AcmIcpcTeam.stringToBits("11")));
+        assertEquals("1110110", bitsToString(AcmIcpcTeam.stringToBits("1110110")));
+        assertEquals(BIG_BITS, bitsToString(AcmIcpcTeam.stringToBits(BIG_BITS)));
     }
 }
